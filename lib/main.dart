a8e735986cd428a075f69f76c0d6f662e4744b96
MElkomey <mm6231169@gmail.com>
2023-05-13 18:12:08 +0300

    getting android battery status

---
+++ lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  static const platform = MethodChannel('samples.flutter.dev/battery');
+
+  // Get battery level.
+  String batteryLevel = 'Unknown battery level.';
+
+  void getBatteryLevel() {
+    platform.invokeMethod('getBatteryLevel').then((value) {
+      setState(() {
+        batteryLevel = 'Battery level at $value % .';
+      });
+
+    }).catchError((error) {
+      setState(() {
+        batteryLevel = "Failed to get battery level: '${error.message}'.";
+      });
+
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            ElevatedButton(
+              onPressed: getBatteryLevel,
+              child: const Text('Get Battery Level'),
+            ),
+            Text(batteryLevel),
+          ],
+        ),
+      ),
+    );
+  }
+
+
+}
